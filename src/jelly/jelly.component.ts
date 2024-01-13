1fbe5f11c5188f90d62f5a1a5255e4fb3c8f7b19
Heidi Michel <heidi_michel@hotmail.com>
2024-01-13 23:53:32 +0100

    Move side menu and search results inside search page

--- src/jelly/jelly.component.ts
+++ src/jelly/jelly.component.ts
@@ -3,14 +3,17 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { JellyTopMenuComponent } from "./top-menu/jelly-top-menu.component";
 import { JellyFooterComponent } from "./footer/jelly-footer.component";
-import { JellySideMenuComponent } from "./side-menu/jelly-side-menu.component";
+import { JellySearchComponent } from './search/search.component';
+import { JellyAboutComponent } from './about/about.component';
+import { JellyHomeComponent } from './home/home.component';
+import { JellyLoginComponent } from './login/login.component';
 
 @Component({
     selector: 'jelly-root',
     standalone: true,
     templateUrl: './jelly.component.html',
     styleUrl: './jelly.component.scss',
-    imports: [CommonModule, RouterOutlet, JellyTopMenuComponent, JellyFooterComponent, JellySideMenuComponent]
+    imports: [CommonModule, RouterOutlet, JellyTopMenuComponent, JellyFooterComponent, JellySearchComponent, JellyAboutComponent, JellyHomeComponent, JellyLoginComponent]
 })
 export class JellyComponent {
   title = 'jelly-gallery';
