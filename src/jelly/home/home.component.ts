d30610df73eafb191acdccea4bd9f70a7ebbd4c0
Heidi Michel <heidi_michel@hotmail.com>
2024-01-13 22:50:33 +0100

    Add new pages/components

---
+++ src/jelly/home/home.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'jelly-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './home.component.html',
+})
+export class HomeComponent {
+
+}
