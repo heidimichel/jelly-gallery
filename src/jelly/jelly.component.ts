ad35b78ce383a598cf20b8ed05f0b7367c40d339
Heidi Michel <heidi_michel@hotmail.com>
2024-01-14 10:59:20 +0100

    Workaround while routing is not 100% functional

--- src/jelly/jelly.component.ts
+++ src/jelly/jelly.component.ts
@@ -1,20 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet, Routes } from '@angular/router';
 import { JellyTopMenuComponent } from "./top-menu/jelly-top-menu.component";
 import { JellyFooterComponent } from "./footer/jelly-footer.component";
 import { JellySearchComponent } from './search/search.component';
 import { JellyAboutComponent } from './about/about.component';
 import { JellyHomeComponent } from './home/home.component';
 import { JellyLoginComponent } from './login/login.component';
+import { JellyFlavoursComponent } from './flavours/flavours.component';
 
 @Component({
     selector: 'jelly-root',
     standalone: true,
     templateUrl: './jelly.component.html',
     styleUrl: './jelly.component.scss',
-    imports: [CommonModule, RouterOutlet, JellyTopMenuComponent, JellyFooterComponent, JellySearchComponent, JellyAboutComponent, JellyHomeComponent, JellyLoginComponent]
+    imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, JellyTopMenuComponent, JellyFooterComponent, JellySearchComponent, JellyAboutComponent, JellyHomeComponent, JellyLoginComponent]
 })
 export class JellyComponent {
   title = 'jelly-gallery';
 }
+
+export const routes: Routes = [
+  { path: 'home', component: JellyHomeComponent },
+  { path: 'about', component: JellyAboutComponent },
+  { path: 'flavours', component: JellyFlavoursComponent },
+  {
+      path: 'search',
+      component: JellySearchComponent,
+  },
+  { path: 'login', component: JellyLoginComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+]
