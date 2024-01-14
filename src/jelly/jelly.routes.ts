62006a42f8329adda3cdb520ecddb59ac332a1b7
Heidi Michel <heidi_michel@hotmail.com>
2024-01-14 10:58:51 +0100

    Add routing

--- src/jelly/jelly.routes.ts
+++ src/jelly/jelly.routes.ts
@@ -1,8 +1,33 @@
 import { Routes } from '@angular/router';
+import { JellyAboutComponent } from './about/about.component';
+import { JellyHomeComponent } from './home/home.component';
+import { JellyLoginComponent } from './login/login.component';
+import { JellySearchComponent } from './search/search.component';
+import { JellyFlavoursComponent } from './flavours/flavours.component';
 
-export const routes: Routes = [];
-//How to add routing here??
-// todo: add routing
+export const routes: Routes = [
+    { path: 'home', component: JellyHomeComponent },
+    { path: 'about', component: JellyAboutComponent },
+    { path: 'flavours', component: JellyFlavoursComponent },
+    {
+        path: 'search',
+        component: JellySearchComponent,
+        /*children: [
+            {
+              path: 'child-a',
+              component: ChildAComponent,
+            },
+            {
+              path: 'child-b',
+              component: ChildBComponent,
+            },
+          ],*/
+    },
+    { path: 'login', component: JellyLoginComponent },
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    //{ path: '**', component: PageNotFoundComponent },
+    // todo: create 404 page. this should always be the last one on the routing.
+];
 /*
 Schematics (or, what I want to do):
 - main component (jelly)
