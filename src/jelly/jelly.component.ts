2eaf7e8050ff8578d3f2651c868f4f6c3d709458
Heidi Michel <heidi_michel@hotmail.com>
2024-01-14 11:15:43 +0100

    Remove extra imports

--- src/jelly/jelly.component.ts
+++ src/jelly/jelly.component.ts
@@ -2,18 +2,13 @@ import { Component, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink, RouterLinkActive, RouterOutlet, Routes } from '@angular/router';
 import { JellyFooterComponent } from "./footer/jelly-footer.component";
-import { JellySearchComponent } from './search/search.component';
-import { JellyAboutComponent } from './about/about.component';
-import { JellyHomeComponent } from './home/home.component';
-import { JellyLoginComponent } from './login/login.component';
-import { JellyFlavoursComponent } from './flavours/flavours.component';
 
 @Component({
     selector: 'jelly-root',
     standalone: true,
     templateUrl: './jelly.component.html',
     styleUrl: './jelly.component.scss',
-    imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, JellyFooterComponent, JellySearchComponent, JellyAboutComponent, JellyHomeComponent, JellyLoginComponent, JellyFlavoursComponent]
+    imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, JellyFooterComponent ]
 })
 export class JellyComponent {
   title = 'jelly-gallery';
