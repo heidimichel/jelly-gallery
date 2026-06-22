d2f9d34b7c3725c9f0dd8e4d349ebea501cbc1a2
heidimichel <heidi_michel@hotmail.com>
2026-06-22 17:00:45 +0200

    Add methods to set the theme on the app

--- src/app/flavours/flavours.ts
+++ src/app/flavours/flavours.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Inject, Renderer2 } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'jelly-flavours',
@@ -6,6 +7,26 @@ import { Component } from '@angular/core';
   templateUrl: './flavours.html',
   styleUrl: './flavours.scss',
 })
-export class JellyFlavoursComponent {
-  //TODO: add methods onClick so each color will trigger a class on the application. Only one class can be triggered at a time.
+export class JellyFlavoursComponent implements OnInit {
+  private flavours = ['grape','blueberry','sky','kiwi','ananas','orange','cherry','strawberry','peach'];
+
+  constructor(private renderer: Renderer2, @Inject(DOCUMENT) private document: Document) {}
+
+  ngOnInit() {
+    const saved = localStorage.getItem('jelly-flavour');
+    if (saved && this.flavours.includes(saved)) {
+      this.setBodyClass(saved);
+    }
+  }
+
+  chooseTheme(flavour: string) {
+    if (!flavour) return;
+    this.setBodyClass(flavour);
+    localStorage.setItem('jelly-flavour', flavour);
+  }
+
+  private setBodyClass(flavour: string) {
+    this.flavours.forEach(f => this.renderer.removeClass(this.document.body, f));
+    this.renderer.addClass(this.document.body, flavour);
+  }
 }
