ff3a7c1a51eeb97eeb146fb4fc0f44968cf4dae6
heidimichel <heidi_michel@hotmail.com>
2026-06-22 16:32:44 +0200

    Spread CSS from styles to the components themselves, add To-Do's

--- src/app/flavours/flavours.ts
+++ src/app/flavours/flavours.ts
@@ -6,4 +6,6 @@ import { Component } from '@angular/core';
   templateUrl: './flavours.html',
   styleUrl: './flavours.scss',
 })
-export class JellyFlavoursComponent {}
+export class JellyFlavoursComponent {
+  //TODO: add methods onClick so each color will trigger a class on the application. Only one class can be triggered at a time.
+}
